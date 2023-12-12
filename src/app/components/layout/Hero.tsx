517b6c0303dc6b03c16d55c0af3f5171bca2e5fd
Leonardo Cintra <leonardo.ncintra@outlook.com>
2023-12-12 11:38:57 -0300

    ✨ feat: initial hero

---
+++ src/app/components/layout/Hero.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import RightIcon from "../icons/RigthIcon";
+
+export default function Hero() {
+  return (
+    <section className="hero">
+      <div className="py-12">
+        <h1 className="text-4xl font-semibold">
+          Tudo <br /> é melhor <br /> com {" "}
+          <span className="text-primary">Pizza</span>
+        </h1>
+        <p className="my-4 text-gray-500 text-sm">
+          Pizza é o que esta faltando para fazer seu dia ficar completo. Uma
+          simples deliciosidade para sua vida
+        </p>
+        <div className="flex gap-4">
+          <button className="flex gap-2 bg-primary text-white px-4 py-3 rounded-full uppercase text-sm items-center">
+            Fazer meu pedido <RightIcon classname="w-6 h-6" />
+          </button>
+          <button className="flex gap-2 py-2 text-gray-600 font-semibold items-center">
+            Mais detalhes <RightIcon classname="w-6 h-6" />
+          </button>
+        </div>
+      </div>
+      <div className="relative">
+        <Image
+          src={"/pizza.png"}
+          alt="pizza"
+          fill
+          style={{
+            objectFit: "contain",
+          }}
+        />
+      </div>
+    </section>
+  );
+}
