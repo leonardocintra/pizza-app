fc75461f63cec82a8df2b3349cb515e3d55ca780
Leonardo Cintra <leonardo.ncintra@outlook.com>
2023-12-27 23:30:43 -0300

    🔧 remove secret pra ver se funciona

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -7,7 +7,6 @@ import { Adapter } from "next-auth/adapters";
 
 
 const handler = NextAuth({
-  secret: process.env.SECRET,
   adapter: MongoDBAdapter(clientPromise) as Adapter,
   providers: [
     GoogleProvider({
